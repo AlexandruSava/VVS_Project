574e1b8c795f956d6ddbf9945b89dc6718d32be8
alexandru.sava.1999 <alexandru.sava.1999@gmail.com>
2021-10-20 23:47:02 +0300

    feat: add contact page and change some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'offers#index'
+
+  get 'contact' => 'pages#contact'
+
   resources :offers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
